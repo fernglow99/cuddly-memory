cb4ed9665c85935cbc9b85d44f86ab50a05c1cc6
lowkaiferng <lowkaiferng@gmail.com>
2016-09-24 00:57:29 -0400

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $('form').submit(function() {
+        var form = $(this);
+        event.preventDefault();
+        var formData = form.serializeArray();
+        formData.forEach(function(data) {
+            if (data.name == 'username') {
+                if (data.value.length < 3) {
+                   alert("Username too short.");
+                }
+            }
+        });
+        var settings = {
+            'method': form.attr('method'),
+            'url': form.attr('action'),
+            'data': form.serialize(),
+        };
+        $.ajax(settings).done(function(msg){
+          alert(msg);  
+        });
+    });
+});
---
+++ register.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+
+$username = htmlentities($_POST['username']);
+$password = crypt($_POST['password'], CRYPT_BLOWFISH);
+
+$db_host = getenv('DB_HOSTNAME');
+$db_name = getenv('DB_NAME');
+$db_user = getenv('DB_USER');
+$db_pass = getenv('DB_PASS');
+
+$dbh = new PDO('sqlsrv:server = ' . $db_host . '; Database = ' . $db_name, $db_user, $db_pass);
+$stmt  = $dbh->prepare("SELECT * FROM `users` where `username` = ?");
+$stmt->execute([$username]);
+
+if($stmt->rowCount()) { //Username taken
+  
+		echo 'Username' . $username . ' is taken.';	
+}
+else{ //Execute registration
+  $stmt2 = $dbh->prepare("INSERT INTO `users` (username, password) VALUES (?,?)");
+	$stmt2->execute([$username, $password]);
+
+	echo 'User Registered successfully';
+
+}
+
+
+
+
+
+
